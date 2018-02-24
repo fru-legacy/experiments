793ca31ad39080de82e4a1bda2a54ab5cde51d9e
Florian Rüberg <florian.rueberg@gmail.com>
2018-02-24 02:22:36 +0100

    Cleanup structure example

--- helper/decorators.py
+++ helper/decorators.py
@@ -2,70 +2,30 @@ import functools
 import tensorflow
 
 
-def _allow_decorator_call_without_parentheses(function):
+def _allow_decorator_call_without_parentheses(f):
     """A decorator decorator, allowing to use the decorator to be used without
     parentheses if not arguments are provided. All arguments must be optional.
     """
 
-    @functools.wraps(function)
+    @functools.wraps(f)
     def decorator(*args, **kwargs):
         if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
-            return function(args[0])
+            return f(args[0])
         else:
-            return lambda wrapee: function(wrapee, *args, **kwargs)
+            return lambda wrapee: f(wrapee, *args, **kwargs)
+
     return decorator
 
 
 @_allow_decorator_call_without_parentheses
-class _CachedProperty(object):
-    """A decorator that converts a function into a lazy property.  The
-    function wrapped is called the first time to retrieve the result
-    and then that calculated result is used the next time you access
-    the value
+def variable_scope(f, scope=None, *args, **kwargs):
+    """ Wrap the functions body in a tf.variable_scope(). If this decorator
+    is used with arguments, they will be forwarded to the variable scope.
+    The scope name defaults to the name of the wrapped function.
     """
 
-    # implementation detail: this property is implemented as non-data
-    # descriptor.  non-data descriptors are only invoked if there is
-    # no entry with the same name in the instance's __dict__.
-    # this allows us to completely get rid of the access function call
-    # overhead.  If one choses to invoke __get__ by hand the property
-    # will still work as expected because the lookup logic is replicated
-    # in __get__ for manual invocation.
+    name = scope or f.__name__.replace('_', '')
 
-    _MISSING = object()
-
-    def __init__(self, func, always_recreated=False, name=None, doc=None):
-        self.__name__ = name or func.__name__
-        self.__module__ = func.__module__
-        self.__doc__ = doc or func.__doc__
-        self.func = func
-        self.always_recreated = always_recreated
-        #if not func.container:
-        #    func.container = []
-        #func.container.
-
-    def __get__(self, obj, type=None):
-        if obj is None:
-            return self
-        value = obj.__dict__.get(self.__name__, self._MISSING)
-        if value is self._MISSING:
-            value = self.func(obj)
-            if self.always_recreated:
-                return value
-            obj.__dict__[self.__name__] = value
-        return value
-
-
-@_allow_decorator_call_without_parentheses
-def property_scoped(f, scope=None, always_recreated=False, *args, **kwargs):
-    """ The operations added by the function live within a tf.variable_scope(). If
-    this decorator is used with arguments, they will be forwarded to the
-    variable scope. The scope name defaults to the name of the wrapped
-    function.
-    """
-    name = scope or f.__name__
-
-    @_CachedProperty(always_recreated)
     @functools.wraps(f)
     def decorator(self):
         with tensorflow.variable_scope(name, *args, **kwargs):
--- structure.py
+++ structure.py
@@ -1,51 +1,60 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-from helper.decorators import property_scoped, property_initializer
+from helper.decorators import variable_scope
 
 
 class Model:
-    @property_initializer
     def __init__(self, image, label):
         self.image = image
         self.label = label
 
-    @property_scoped(initializer=tf.contrib.slim.xavier_initializer())
-    def prediction(self):
+        self.prediction = self._prediction()
+        self.optimize = self._optimize()
+        self.error = self._error()
+
+    @variable_scope(initializer=tf.contrib.slim.xavier_initializer())
+    def _prediction(self):
         x = self.image
         x = tf.contrib.slim.fully_connected(x, 200)
         x = tf.contrib.slim.fully_connected(x, 200)
         x = tf.contrib.slim.fully_connected(x, 10, tf.nn.softmax)
         return x
 
-    @property_scoped
-    def optimize(self):
+    @variable_scope
+    def _optimize(self):
         logprob = tf.log(self.prediction + 1e-12)
         cross_entropy = -tf.reduce_sum(self.label * logprob)
         optimizer = tf.train.RMSPropOptimizer(0.03)
         return optimizer.minimize(cross_entropy)
 
-    @property_scoped
-    def error(self):
+    @variable_scope
+    def _error(self):
         mistakes = tf.not_equal(tf.argmax(self.label, 1), tf.argmax(self.prediction, 1))
         return tf.reduce_mean(tf.cast(mistakes, tf.float32))
 
 
 def main():
     mnist = input_data.read_data_sets('/data/MNIST-data', one_hot=True)
+    # mnist.image
     image = tf.placeholder(tf.float32, [None, 784])
     label = tf.placeholder(tf.float32, [None, 10])
+    # mnist.next_batch
+    def next_batch(size):
+        images, labels = mnist.train.next_batch(size)
+        return {image: images, label: labels}
+    def testing():
+        return {image: mnist.test.images, label: mnist.test.labels}
+
     model = Model(image, label)
     sess = tf.Session()
     sess.run(tf.initialize_all_variables())
 
     for _ in range(10):
-        images, labels = mnist.test.images, mnist.test.labels
-        error = sess.run(model.error, {image: images, label: labels})
+        error = sess.run(model.error, {**testing()})
         print('Test error {:6.2f}%'.format(100 * error))
         for _ in range(60):
-            images, labels = mnist.train.next_batch(100)
-            sess.run(model.optimize, {image: images, label: labels})
+            sess.run(model.optimize, {**next_batch(100)})
 
 
 if __name__ == '__main__':
-  main()
+    main()
