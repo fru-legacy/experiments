05a8a7d515e4b76b70019b517944c7c9f0ae0c0f
Florian Rüberg <florian.rueberg@gmail.com>
2018-03-26 13:25:12 +0200

    Add normal encoding

--- Noise0Autoencoder.py
+++ Noise0Autoencoder.py
@@ -11,13 +11,41 @@ class Noise0Autoencoder(BaseNetwork):
     def __init__(self, data):
         super(Noise0Autoencoder, self).__init__()
         self.data = data
-        self.base = int(np.prod(self.data.info.dim_image))
+        self.image_normal_dims = [int(np.prod(self.data.info.dim_image)), 8, 2]
+        self.image_normal_base = int(np.prod(self.image_normal_dims))
         #self.add_control_linear_regression('latent', self.latent)
         self.optimizers.append(self.generator_optimize)
         self.optimizers.append(self.generator_summary)
 
     # TODO: how will epsilon affect reconstruction
     epsilon = 1.0 / (255 * 8)
+
+    @scope(cached_property=True)
+    def image_normal_encoding(self, repeat=4):
+        direction = tf.tile(self.data.image_byte, [1, 1, repeat]) * 2 - 1
+        print(direction.get_shape())
+        normal = tf.abs(tf.random_normal(tf.shape(direction)))
+        return tf.layers.flatten(tf.multiply(normal, direction))
+
+    def image_normal_cross_entropy(self, result):
+        assert(list(result.get_shape()) == [-1, self.image_normal_base])
+        one_hot_bytes = tf.one_hot(self.data.image_byte, 2)
+        print(self.data.image_byte.get_shape(), one_hot_bytes.get_shape())
+        one_hot_result = tf.reshape(result, [-1, self.image_normal_base/2, 2])
+        return tf.losses.softmax_cross_entropy(one_hot_bytes, one_hot_result)
+
+    def image_normal_restored(self, result):
+        max = tf.argmax(tf.reshape(result, [-1] + self.image_normal_dims), -1)
+        print(max.get_shape())
+        return tf.reshape(Noise0Autoencoder.pack_bits_to_int8(max), [-1] + self.data.info.dim_image)
+
+    @staticmethod
+    def pack_bits_to_int8(values):
+        assert(values.get_shape()[-1] == 8)
+        bases = 2 ** tf.range(8)
+        converted = tf.reduce_sum(tf.reshape(values, [-1, 8]) * bases, 1)
+        print(values.get_shape()[0:-2])
+        return tf.reshape(converted, values.get_shape()[0:-2])
 
     # Autoencoder: image -> latent + epsilon noise -> image
 
@@ -32,7 +60,7 @@ class Noise0Autoencoder(BaseNetwork):
 
     @scope(cached_property=True)
     def latent(self):
-        x = self.data.image_flat
+        x = self.data.image_flat#_normal_encoding
         #x = self.fully_connected(x, self.base)
         #x = self.fully_connected(x, self.base * 2)
         #x = self.fully_connected(x, self.base * 2)
@@ -51,7 +79,7 @@ class Noise0Autoencoder(BaseNetwork):
         x = self.latent_minimum_noise
         #x = self.fully_connected(x, self.base * 2)
         #x = self.fully_connected(x, self.base * 2)
-        x = self.fully_connected(x, self.base)
+        x = self.fully_connected(x, int(np.prod(self.data.info.dim_image)))
         return x
 
     @scope(cached_property=True)
--- main.py
+++ main.py
@@ -7,4 +7,4 @@ from Noise0Autoencoder import Noise0Autoencoder
 
 data = tf_data.fashion_mnist('/data/tf-data').placeholder()
 model = Noise0Autoencoder(data)
-model.run(lambda: data.train(batch_size=1024), 0, steps=500, is_training=True)
+model.run(lambda: data.train(batch_size=256), 0, steps=1, is_training=True)
