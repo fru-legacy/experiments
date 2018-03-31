9b6949067ae4a03091ed6bf43e3392a8e54ac508
Florian Rüberg <florian.rueberg@gmail.com>
2018-03-31 20:52:36 +0200

    Autoencoder
    Mean Squared Error - batch size is squared / resource exhausted

--- Noise0Autoencoder.py
+++ Noise0Autoencoder.py
@@ -9,104 +9,64 @@ from tested.mmd_kernel import compute_mmd
 
 class Noise0Autoencoder(BaseNetwork):
     def __init__(self, data):
-
-        self.data = data
-        super(Noise0Autoencoder, self).__init__()
+        super(Noise0Autoencoder, self).__init__(data)
 
         # Parameter
-
         self.latent_stddev = 1.0 / (255 * 8)
         self.learning_rate = 1e-5
-        self.restore_compare_k = 4
-        self.restore_stddev = 0.2
-        self.restore_classes = 8
-        self.restore_steps = 2
 
-        # Helper
-
+        # Data
+        self.patches = self.data.patches([4, 4], auxiliary_max_count=0)
+        self.generator_size = self.patches.size
         self.base_pixel_count = int(np.prod(self.data.info.dim_image))
-        self.generator_size = self.base_pixel_count * 2
 
         # Network
-
-        self.latent_input = tf.layers.flatten(self.data.image_byte * 2 - 1)
-        self.create_generator_summary()
         self.optimizers.append(self.optimize_generator)
+        self.create_generator_summary()
 
-    # Selu needs stddev = 1 and mean = 0
-
-    def image_normal_encoding_single(self):
-        x = tf.cast(self.data.image, tf.float32) / 255.0
-        return (x + 1 / 2) * 1.15
-
-    @scope(cached_property=True)
-    def latent_input_normalized(self):
-        e1 = tf.expand_dims(self.image_normal_encoding_single(), -1)
-        e2 = tf.expand_dims(self.image_normal_encoding_single(), -1)
-        return tf.reshape(tf.concat([e1, -e2], axis=-1), [-1, self.base_pixel_count, 2])
-
-    # Log mean, variance and the histogram of a tensor
-
-    @staticmethod
-    def log_distribution(name, data):
-        mean, variance = tf.nn.moments(data, axes=list(range(len(data.get_shape()))))
-        tf.summary.scalar('mean ' + name, mean)
-        tf.summary.scalar('variance ' + name, variance)
-        tf.summary.histogram('normal' + name, data)
-
-    # Use this to control feature usability
-
-    def add_control_classification(self, name, latent):
-        with tf.variable_scope('control_classification_' + name):
-            prediction = self.fully_connected(latent, self.data.info.label_count, plain=True)
-            cross_entropy = tf.losses.softmax_cross_entropy(self.data.label_one_hot, prediction)
-            tf.summary.scalar('control classification ' + name, cross_entropy)
-            in_scope = self.get_current_trainable_vars(expected_count=2)
-            optimize = tf.train.AdamOptimizer().minimize(cross_entropy, var_list=in_scope)
-            self.optimizers.append(optimize)
+        test1 = tf.model_variables()
+        test2 = tf.global_variables()
+        test3 = tf.local_variables()
+        test4 = tf.trainable_variables()
+        print(test1, test2, test3, test4)
 
     # Autoencoder: image -> latent + epsilon noise -> image
 
     @scope(cached_property=True)
+    def input_normalized(self):
+        return tf.cast(self.patches.data, tf.float32) / 255.0
+
+    @scope(cached_property=True)
     def latent(self):
-        x = self.latent_input_normalized
-        Noise0Autoencoder.log_distribution('input', x)
-        x = self.fully_connected(x, 40)
-        x = self.fully_connected(x, 40)
-        x = self.fully_connected(x, 40)
-        Noise0Autoencoder.log_distribution('latent', x)
+        x = self.input_normalized
+        #x = self.fully_connected(x, 40)
+        #x = self.fully_connected(x, 40)
+        #x = self.fully_connected(x, 40)
+        tf.summary.histogram('latent', x)
         return x
 
     @scope(cached_property=True)
     def generator(self):
         x = self.latent_minimum_noise
-        x = self.fully_connected(x, 40)
-        x = self.fully_connected(x, 40)
-        x = self.fully_connected(x, 2, plain=True)
-        Noise0Autoencoder.log_distribution('output', x)
+        #x = self.fully_connected(x, 40)
+        #x = self.fully_connected(x, 40)
+        #x = self.fully_connected(x, 40)
+        x = self.fully_connected(x, self.generator_size, plain=True)
+        tf.summary.histogram('generator', x)
         return x
 
     @scope(cached_property=True)
     def latent_minimum_noise(self):
         return self.latent + tf.random_normal(tf.shape(self.latent), stddev=self.latent_stddev)
 
-    def extract_image(self, x):
-        x = tf.slice(tf.reshape(x, [-1, 2]), [0, 0], [-1, 1])
-        return tf.reshape(x, [-1] + self.data.info.dim_image)
-
     def create_generator_summary(self):
-        tf.summary.image('restored', self.extract_image(self.generator), 1)
-        tf.summary.image('original', self.extract_image(self.latent_input_normalized), 1)
+        self.patches.restored_image_summary('restored', self.generator)
+        tf.summary.image('original', self.data.image, 1)
 
     @scope(cached_property=True)
     def optimize_generator(self):
-        x = tf.losses.mean_squared_error(self.latent_input_normalized, self.generator)
-        tf.summary.scalar('cross_entropy', x)
-        return tf.train.RMSPropOptimizer(self.learning_rate).minimize(x)
-
-    @scope(cached_property=True)
-    def optimize_labels(self):
-        generated_labels = self.fully_connected(self.generator, 10, plain=True)
-        x = tf.losses.softmax_cross_entropy(self.data.label_one_hot, generated_labels)
-        tf.summary.scalar('cross_entropy', x)
-        return tf.train.RMSPropOptimizer(self.learning_rate).minimize(x)
+        x = tf.losses.mean_squared_error(self.input_normalized, self.generator)
+        y = compute_mmd(self.input_normalized, self.generator)
+        tf.summary.scalar('cross entropy restore', x)
+        tf.summary.scalar('mmd', y)
+        return tf.train.GradientDescentOptimizer(self.learning_rate).minimize(x)
--- helper/BaseNetwork.py
+++ helper/BaseNetwork.py
@@ -7,11 +7,11 @@ from helper.helper import get_initialized_session
 
 
 class BaseNetwork:
-    def __init__(self):
+    def __init__(self, data):
         np.random.seed(0)
         tf.set_random_seed(0)
 
-        self.shape = [12, 64, 128]
+        self.data = data
         self.is_training = tf.placeholder_with_default(True, shape=())
         self.log_name = 'log-' + self.__class__.__name__
 
@@ -33,16 +33,24 @@ class BaseNetwork:
     def fully_connected(self, x, size, plain=False):
         if plain:
             return tf.layers.dense(x, size)
-
-        return tf.layers.dense(x, size, activation=selu, kernel_initializer=initializer)
-        defaults = {'activation_fn': selu, 'weights_initializer': initializer}
-        return tf.contrib.layers.fully_connected(x, size, **defaults) #biases_initializer=None
+        return tf.layers.dense(x, size, activation=tf.nn.relu, kernel_initializer=initializer)
 
     def get_current_trainable_vars(self, expected_count=None):
         prefix = tf.get_variable_scope().name
         result = [v for v in tf.trainable_variables() if v.name.startswith(prefix)]
         assert expected_count is None or len(result) == expected_count
         return result
+
+    # Use this to control feature usability
+
+    def add_control_classification(self, name, latent):
+        with tf.variable_scope('control_classification_' + name):
+            prediction = self.fully_connected(latent, self.data.info.label_count, plain=True)
+            cross_entropy = tf.losses.softmax_cross_entropy(self.data.label_one_hot, prediction)
+            tf.summary.scalar('control classification ' + name, cross_entropy)
+            in_scope = self.get_current_trainable_vars(expected_count=2)
+            optimize = tf.train.AdamOptimizer().minimize(cross_entropy, var_list=in_scope)
+            self.optimizers.append(optimize)
 
     def run(self, data_generator, iteration, steps, is_training):
         if not self.has_run_initialized:
@@ -51,7 +59,7 @@ class BaseNetwork:
             self.summary = tf.summary.merge_all()
             self.session = get_initialized_session(disable_gpu=False)
             self.summary_writer = tf.summary.FileWriter('./log/' + self.log_name, self.session.graph)
-            #self.saver = tf.train.Saver()
+            self.saver = tf.train.Saver()
             self.saver_path = './log/checkpoint_' + self.log_name
 
             if os.path.exists(self.saver_path):
@@ -63,5 +71,5 @@ class BaseNetwork:
             data = data_generator()
             log, _ = self.session.run([self.summary, self.execute], {**data, self.is_training: is_training})
             self.summary_writer.add_summary(log, iteration+e)
-            #self.saver.save(self.session, self.saver_path + '/model.ckpt')
+            self.saver.save(self.session, self.saver_path + '/model.ckpt')
             print('Iteration', e)
